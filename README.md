8004928698b0ea19620a4a488f1dd9ef46a6933e
mikeascalon <mikeascalon@yahoo.com>
2024-02-21 17:28:55 -0800

    added auth and guicorn server

--- README.md
+++ README.md
@@ -1,30 +1,80 @@
-# LAB - Class 32
+# LAB - Class 33
 
-Project: Permissions & Postgresql
+Project: Authentication & Production Server
 
 Author: Michelangelo Ascalon
 
 ## Overview
 
-Added permissions to ensure that only authenticated users have access to the API. Implement a custom permission to allow only appropriate users to update or delete entries. Added functionality to switch users directly from the browsable API, enhancing the user experience and testing capabilities. Incorporate PostgreSQL in your Docker setup. 
+This project enhances an existing API by adding JWT Authentication to ensure secure access to resources. Additionally, it transitions the development server to Gunicorn, a production-grade WSGI server, for improved performance and scalability. The project also includes configuration for Docker to simplify deployment and development workflows.
 
+### Setup
 
-PORT - [Port Number](http://127.0.0.1:8000/api/v1/coffees/)
+Prerequisites
 
-### Running the Application
+1. Docker and Docker Compose installed on your machine.
 
-1. **Docker**: To run the application using Docker, ensure Docker is installed on your machine. Then execute:
+2. Basic understanding of Docker, Django, and Django REST Framework.
+   
+#### Installation
+
+1.Clone the repository to your local machine.
+
+2. Navigate to the project directory.
+
+3.Build and start the containers with Docker Compose.
 
 ```bash
 docker-compose up --build
+
 ```
 
-#### How to Use the API
+#### Access and Testing
 
-Access the list of coffees or create a new coffee entry:
+The API is accessible at: http://127.0.0.1:8000/api/v1/coffees/
 
-GET and POST requests to http://127.0.0.1:8000/api/v1/coffees/
+Using the API
 
-Retrieve, update, or delete a specific coffee entry:
+* Get Tokens: POST request to `/api/token/` with username and password to receive access and refresh JWT tokens.
+  
+* Refresh Tokens: POST request to `/api/token/refresh/` with refresh token to receive a new access token.
 
-GET, PUT, PATCH, and DELETE requests to http://127.0.0.1:8000/api/v1/coffees/
+CRUD Operations for Coffees Resource:
+
+* Create (Token Required): POST to `/api/v1/coffees/` with coffee details.
+  
+* Read List: GET `/api/v1/coffees/` to retrieve a list of coffees.
+  
+* Read Detail: GET `/api/v1/coffees/<id>/` to retrieve coffee details.
+  
+* Update (Token Required): PUT or PATCH `/api/v1/coffees/<id>/` with updated coffee details.
+  
+* Delete (Token Required): DELETE `/api/v1/coffees/<id>/`.
+
+bash
+
+PORT - [Port Number](http://127.0.0.1:8000/api/v1/coffees/)
+
+Manual Testing
+Manual testing can be done using tools like httpie or ThunderClient, . Below are examples of how to manually test the endpoints:
+
+```bash
+# Get Tokens
+http POST http://127.0.0.1:8000/api/token/ username=<username> password=<password>
+
+# Refresh Token
+http POST http://127.0.0.1:8000/api/token/refresh/ refresh=<refresh_token>
+
+# Access Protected Route
+http GET http://127.0.0.1:8000/api/v1/coffees/ "Authorization:Bearer <access_token>"
+
+```
+Replace <username>, <password>, <refresh_token>, and <access_token> with actual values
+
+##### Notes
+
+* Ensure Docker is running on your machine before starting the application.
+  
+* Adjust docker-compose.yml according to your database preference (SQLite or PostgreSQL).
+
+* Static files are served using Whitenoise; ensure your static file paths are correctly set in settings for production.
--- coffees/tests.py
+++ coffees/tests.py
@@ -1,3 +1,94 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Coffee
 
-# Create your tests here.
+class CoffeeTests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create a user
+        testuser1 = get_user_model().objects.create_user(username="testuser1", password="pass")
+        testuser1.save()
+
+        # Create a coffee instance
+        test_coffee = Coffee.objects.create(
+            owner=testuser1,
+            name="Espresso",
+            description="A full-flavored, concentrated form of coffee.",
+            price=2.99
+        )
+        test_coffee.save()
+
+    def setUp(self):
+        # Log in the user for each test
+        self.client.login(username="testuser1", password="pass")
+
+    def test_coffees_model(self):
+        coffee = Coffee.objects.get(id=1)
+        actual_owner = str(coffee.owner)
+        actual_name = str(coffee.name)
+        actual_description = str(coffee.description)
+        actual_price = coffee.price
+        self.assertEqual(actual_owner, "testuser1")
+        self.assertEqual(actual_name, "Espresso")
+        self.assertEqual(actual_description, "A full-flavored, concentrated form of coffee.")
+        self.assertEqual(actual_price, 2.99)
+
+    def test_get_coffee_list(self):
+        url = reverse("coffee_list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        coffees = response.data
+        self.assertEqual(len(coffees), 1)
+        self.assertEqual(coffees[0]["name"], "Espresso")
+
+    def test_get_coffee_by_id(self):
+        url = reverse("coffee_detail", args=(1,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        coffee = response.data
+        self.assertEqual(coffee["name"], "Espresso")
+
+    def test_create_coffee(self):
+        url = reverse("coffee_list")
+        data = {
+            "owner": 1,
+            "name": "Latte",
+            "description": "Espresso mixed with steamed milk",
+            "price": "3.50"
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        coffees = Coffee.objects.all()
+        self.assertEqual(len(coffees), 2)
+        self.assertEqual(Coffee.objects.get(id=2).name, "Latte")
+
+    def test_update_coffee(self):
+        url = reverse("coffee_detail", args=(1,))
+        data = {
+            "owner": 1,
+            "name": "Espresso",
+            "description": "Short and strong coffee with a rich flavor",
+            "price": "2.99"
+        }
+        response = self.client.put(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        coffee = Coffee.objects.get(id=1)
+        self.assertEqual(coffee.name, data["name"])
+        self.assertEqual(coffee.owner.id, data["owner"])
+        self.assertEqual(coffee.description, data["description"])
+        self.assertEqual(str(coffee.price), data["price"])
+
+    def test_delete_coffee(self):
+        url = reverse("coffee_detail", args=(1,))
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        coffees = Coffee.objects.all()
+        self.assertEqual(len(coffees), 0)
+
+    def test_authentication_required(self):
+        self.client.logout()
+        url = reverse("coffee_list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
